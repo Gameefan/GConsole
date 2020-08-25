5d5df03ac25aab40b8206112ca7e1eae9523a309
Gameefan <46563496+Gameefan@users.noreply.github.com>
2020-08-25 17:14:12 +0200

    Project moved to Github
    Code is now on Github

---
+++ GameefanConsole/File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameefanConsole
+{
+	public class File
+	{
+		public string path = "/";
+		public string name = "New File";
+		public string data;
+		public bool canUserRead = true;
+		public bool canUserWrite = true;
+		public bool canUserExecute = false;
+		public string flags = "F";
+		public string ownerID = "root";
+	}
+}
